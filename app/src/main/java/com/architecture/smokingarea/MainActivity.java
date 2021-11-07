84ba5254670752c88a8216edea758a67a9cd5217
its-mingyu <lion9638@naver.com>
2021-11-07 23:17:49 +0900

    2021/11/07 - 2차 커밋, 카카오 Map API 추가, 해시키, 외부 라이브러리 추가

--- app/src/main/java/com/architecture/smokingarea/MainActivity.java
+++ app/src/main/java/com/architecture/smokingarea/MainActivity.java
@@ -1,18 +1,31 @@
 package com.architecture.smokingarea;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
-public class MainActivity extends MapActivity {
-    
-    @Override
-    protected void initView(Bundle savedInstance){
+import java.security.MessageDigest;
 
+public class MainActivity extends AppCompatActivity {
+    public MainActivity(){
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/com/architecture/smokingarea/MapFragment.java
@@ -0,0 +1,30 @@
+package com.architecture.smokingarea;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+
+public class MapFragment extends Fragment {
+    private GoogleMap googleMap;
+
+    public MapFragment() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+}
