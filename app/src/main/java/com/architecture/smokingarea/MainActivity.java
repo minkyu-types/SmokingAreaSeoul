dd91c9f50fa47f3016f731dce14b89aed0edf063
its-mingyu <lion9638@naver.com>
2021-11-09 19:02:06 +0900

    2021/11/09 - 1차 커밋, 구조 변경, 구글->카카오 맵으로 변경

--- app/src/main/java/com/architecture/smokingarea/MainActivity.java
+++ app/src/main/java/com/architecture/smokingarea/MainActivity.java
@@ -3,6 +3,9 @@ package com.architecture.smokingarea;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -12,14 +15,18 @@ import android.content.pm.Signature;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.ViewGroup;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
+import net.daum.mf.map.api.MapView;
+
 import java.security.MessageDigest;
 
 public class MainActivity extends AppCompatActivity {
+
     public MainActivity(){
     }
 
@@ -27,5 +34,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
--- app/src/main/java/com/architecture/smokingarea/MapFragment.java
+++ app/src/main/java/com/architecture/smokingarea/MapFragment.java
@@ -2,6 +2,7 @@ package com.architecture.smokingarea;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
@@ -10,21 +11,63 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class MapFragment extends Fragment {
-    private GoogleMap googleMap;
+import java.util.Map;
 
-    public MapFragment() {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+    private MapView kakaomap = null;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View mapView = inflater.inflate(R.layout.fragment_map, container, false);
+
+        kakaomap = (MapView) mapView.findViewById(R.id.mapView);
+        kakaomap.getMapAsync(this);
+
+        return mapView;
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if(kakaomap!=null){
+            kakaomap.onCreate(savedInstanceState);
+        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+    public void onStart() {
+        super.onStart();
+        kakaomap.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        kakaomap.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        kakaomap.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        kakaomap.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        kakaomap.onDestroy();
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
     }
 }
